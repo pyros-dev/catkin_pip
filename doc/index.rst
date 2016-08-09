ba8a8f1719273f4b1470e5c56d62576c8145d3c0
alexv <asmodehn@gmail.com>
2016-08-09 19:00:26 +0900

    adding specific script for travis checks. added basic doc structure.

---
+++ doc/index.rst
@@ -0,0 +1,24 @@
+.. _index:
+
+Catkin Pip
+==========
+
+This `ROS`_ package allows using pure python package in ROS Systems, in catkin workspaces.
+
+.. include:: weblinks.rst
+
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme_link
+   overview
+   changelog_link
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
---
+++ travis_checks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# These variables need to be setup before calling this script:
+# CI_ROS_DISTRO [indigo | jade]
+# ROS_FLOW [devel | install]
+
+source /opt/ros/$CI_ROS_DISTRO/setup.bash
+mkdir testbuild
+cd testbuild
+cmake ../test -DCMAKE_INSTALL_PREFIX=./install
+if [ "$ROS_FLOW" == "devel" ]; then
+    make -j1
+    source devel/setup.bash
+    make -j1 tests
+    make -j1 run_tests
+    catkin_test_results .
+elif [ "$ROS_FLOW" == "install" ]; then
+    make -j1 install
+    source install/setup.bash
+    nosetests mypippkg
+    py.test --pyargs mypippkg
+fi
