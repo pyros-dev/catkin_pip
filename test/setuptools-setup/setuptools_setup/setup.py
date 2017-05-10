a7fb251e8bdd6ef77fa25a41641b27425a8db829
AlexV <asmodehn@gmail.com>
2017-05-10 17:12:55 +0900

    adding setuptools_setup test project and tests to validate package structure after installation.

---
+++ test/setuptools-setup/setuptools_setup/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='setuptools_setup',
+      version='0.0.1',
+      description='A python package that manages setup.py with setuptools.',
+      author='AlexV',
+      author_email='asmodehn@gmail.com',
+      packages=['sstest'],
+     )
+
