7ee5b0066a5d67ca99dd7b3d8538f731fe665e2a
AlexV <asmodehn@gmail.com>
2017-05-23 19:06:39 +0900

    removing tests with pyros_setup. we DO NOT WANT to depend on it here.

--- test/catkin_pip_pytest/conftest.py
+++ test/catkin_pip_pytest/conftest.py
@@ -2,9 +2,6 @@ import os
 import sys
 import pytest
 
-# Imports CANNOT be in fixtues (pytest would cleanup modules, preventing re-import...)
-import pyros_setup
-
 
 def pytest_addoption(parser):
     parser.addoption("--build-dir", action="store", default="build", help="the build directory")
@@ -68,34 +65,3 @@ def install_space():
     assert os.path.exists(os.path.abspath(os.path.join('install', 'setup.sh'))), "install/setup.bash not found in {0}".format(os.getcwd())
     ws = os.path.abspath('install')
     return ws
-
-@pytest.fixture
-def pyros():
-    # IMPORTANT : Without pyros_setup configuration activated,
-    # the develop packages are set in sys.path AFTER the site-packages and dist-packages
-    # This is because of incompatibilities between ROS and python ways of handling PYTHONPATH.
-    # It is usually not a problem, unless :
-    #  - you are working in a devel space with catkin_pip and expect to use a editable package from there
-    #  - you have the same package installed somewhere else in the PYTHONPATH
-    #    And in that case it might end up being *before* the source dir in sys.path
-    #
-    # => Without pyros_setup, the installed version will be preferred
-    # This is the python (and tools) way of working, since PYTHONPATH purpose is to override file-based setup,
-    # like editable packages.
-    #
-    # => With pyros_setup, the devel version will be preferred
-    # This is the ROS way of working, since PYTHONPATH order determine the order of folder when looking for a package.
-    #
-    # Both will work fine in most cases, but one might want to keep this corner case in mind...
-
-    print("sys.path before pyros_setup {pyros_setup.__file__} :\n{sys.path}".format(**globals()))
-
-    # We need to pass the proper workspace here
-    pyros_setup.configurable_import().configure({
-        'WORKSPACES': [
-            devel_space()
-        ],  # This should be the same as pytest devel_space fixture
-        'DISTRO': pyros_setup.DETECTED_DISTRO,
-    }).activate()
-
-    # We cannot move pyros_setup outside the fixture (pytest would clean it up and break module imports)
